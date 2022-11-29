eae43d7f03ecc09dacd2f84c8b0339a89b4e2450
erokemwa <erokemwa@yahoo.com>
2022-11-29 08:43:53 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) != 'e' and chr(i) != 'q':
-        print('{:c}'.format(i), end='')
+# prints alphabet
+
+for i in range(0, 26):
+    if i != 4 and i != 16:
+        print("{:s}" .format(chr(i + 97)), end='')
