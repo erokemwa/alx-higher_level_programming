b262f943538e4e50ea3e71dd9cbeae65e0b84fc3
erokemwa <erokemwa@yahoo.com>
2023-01-11 01:19:52 -0800

    inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,11 +1,8 @@
- a list of class attributes"""
+#!/usr/bin/python3
+
+"""Returns a list of class attributes"""
 
 
-
-
-
- def lookup(obj):
-
-     """method to looup class attributes"""
-
-         return dir(obj)
+def lookup(obj):
+    """method to looup class attributes"""
+    return dir(obj)
