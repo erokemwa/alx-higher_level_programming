860f249950f6ac15c8bccfca47b709658f114535
erokemwa <erokemwa@yahoo.com>
2023-02-15 10:05:50 -0800

    11-genre_id_all_shows.sql

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,9 @@
+-- Write a script that lists all shows contained in the database hbtn_0d_tvshows.
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+	FROM tv_shows
+	LEFT JOIN tv_show_genres
+	ON tv_shows.id = tv_show_genres.show_id
+	LEFT JOIN tv_genres
+	ON tv_genres.id = tv_show_genres.genre_id
+	ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
