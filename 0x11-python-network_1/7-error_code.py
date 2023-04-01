cd44794aef78e9f0f428124ce25224a9225c4277
erokemwa <erokemwa@yahoo.com>
2023-03-31 19:23:14 -0700

    Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""  displays the value of the X-Request-Id variable
+    found in the header of the response
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
