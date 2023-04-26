eb4c937502f25adfbbc22dde9906d23c397dce8b
erokemwa <erokemwa@yahoo.com>
2023-04-25 19:51:17 -0700

    Loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,41 +1,36 @@
 #!/usr/bin/node
 
+// Import the `fs` module. The `fs` module provides functions for working with files.
+const fs = require('fs');
+
 // Import the `request` module. The `request` module provides functions for making HTTP requests.
 const request = require('request');
 
-// The URL to request.
+// Get the URL from the command line arguments.
 const url = process.argv[2];
+
+// Get the file path from the command line arguments.
+const filePath = process.argv[3];
 
 // Make an HTTP GET request to the URL.
 request(url, (err, res, body) => {
   // Check if there was an error.
   if (err) {
-    // If there was an error, log it to the console.
-    console.error(err);
+    // If there was an error, log it to the console and exit.
+    console.log(err);
     return;
   }
 
-  // Parse the JSON response body.
-  const data = JSON.parse(body);
-
-  // Initialize a counter to keep track of the number of characters who are 18 years old or older.
-  let count = 0;
-
-  // Iterate over the results array.
-  for (let i = 0; i < data.results.length; i++) {
-    // Get the characters array for the current result.
-    const chars = data.results[i].characters;
-
-    // Iterate over the characters array.
-    for (let k = 0; k < chars.length; k++) {
-      // Check if the current character's name includes the string "18".
-      if (chars[k].includes('18')) {
-        // If the current character's name includes the string "18", increment the counter.
-        count++;
-      }
+  // Write the body of the response to the file in the UTF-8 encoding.
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    // Check if there was an error.
+    if (err) {
+      // If there was an error, log it to the console and exit.
+      console.log(err);
+      return;
     }
-  }
 
-  // Log the number of characters who are 18 years old or older.
-  console.log(count);
+    // The file has been written successfully.
+    console.log(`File ${filePath} has been written successfully.`);
+  });
 });
